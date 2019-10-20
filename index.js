c0ac02341830f14824361910fffd408174cd90e2
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2019-10-20 11:20:52 +0200

    Refactored to using promises. Made index.html ordered.

--- index.js
+++ index.js
@@ -9,76 +9,49 @@ const options = {
 
 rp(options)
     .then((data) => {
-        let edges = [];
-        for (let edge of data.result.data.allChallengeNode.edges) {
-            edges.push({
-                block: edge.node.block,
-                slug: edge.node.fields.slug,
-            });
-        };
-        const blocks = getBlocks(edges);
-        blocks.forEach(block => {
-            writeChallenges(block, edges);
-        });
-        // writeChallenges('react', edges);
-    })
-    .catch((error) => {
-        console.log(error);
+        let edges = getEdges(data);
+        let challengePromises = getChallengePromises(edges);
+        console.log('#challengePromises', challengePromises.length);
+        Promise.all(challengePromises)
+            .then(writeHtml)
+            .catch((error) => {
+                console.log('Error', error);
+            })
     });
 
-function getBlocks(edges) {
-    let blocks = [];
-    edges.forEach((edge) => {
-        if (!blocks.includes(edge.block)) {
-            blocks.push(edge.block);
-        }
-    });
-    return blocks;
-}
-
-function writeChallenges(block, edges) {
-    let i = 0;
-    let challenges = [];
-    function next() {
-        if (i < edges.length) {
-            if (edges[i].block === block) {
-                let options = {
-                    url: `https://www.freecodecamp.org/page-data` + edges[i].slug + `/page-data.json`,
-                    json: true
-                }
-                console.log('Requesting', options.url);
-                rp(options)
-                    .then((data) => {
-                        let challenge = {
-                            title: data.result.data.challengeNode.title,
-                            description: data.result.data.challengeNode.description
-                        }
-                        challenges.push(challenge);
-                        i++;
-                        next();
-                    })
-                    .catch((error) => { console.log('Error', error) })
-            } else {
-                i++;
-                next();
-            }
-        } else {
-            writeData(block, challenges);
-        }
-    }
-    return next();
-}
-
-function writeData(block, challenges) {
-    console.log('✅');
+function writeHtml(challenges) {
+    let lastBlockName = '';
     challenges.forEach(challenge => {
-        appendFileSync(
-            `${block}Challenges.html`,
-            `<h2>${challenge.title}</h2>\n${challenge.description}\n`
-        )
+        let challengeNode = challenge.result.data.challengeNode;
+        appendFileSync(`${challengeNode.fields.blockName.replace(/\s+/g, '')}Challenges.html`, `<h2>${challengeNode.title}</h2>\n${challengeNode.description}\n`);
+        if (challengeNode.fields.blockName !== lastBlockName) {
+            appendFileSync('index.html', `<a href="${challengeNode.fields.blockName.replace(/\s+/g, '')}Challenges.html">${challengeNode.fields.blockName} Challenges</a><br/>\n`);
+        }
+        lastBlockName = challengeNode.fields.blockName;
     });
-    appendFileSync(
-        'index.html',
-        `<a href="${block}Challenges.html">${block}Challenges</a><br/>`
-    )
+}
+
+function getChallengePromises(edges) {
+    let challengePromises = [];
+    edges.forEach(edge => {
+        let options = {
+            url: `https://www.freecodecamp.org/page-data` + edge.slug + `/page-data.json`,
+            json: true
+        };
+        challengePromises.push(rp(options));
+    });
+    return challengePromises;
+}
+
+function getEdges(data) {
+    let edges = [];
+    for (let edge of data.result.data.allChallengeNode.edges) {
+        edges.push({
+            superBlock: edge.node.superBlock,
+            block: edge.node.block,
+            blockName: edge.node.fields.blockName,
+            slug: edge.node.fields.slug,
+        });
+    }
+    return edges;
 }
